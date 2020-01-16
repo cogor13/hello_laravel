4dad2ed43f457987e931f2551f1a920719f26789
cjj <2941236242@qq.com>
2020-01-16 17:19:28 +0800

    v5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -88,9 +88,9 @@
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">博客</a>
+                    <a href="https://blog.laravel.com">Blog</a>
                     <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">其他</a>
+                    <a href="https://forge.laravel.com">Other</a>
                     <a href="https://vapor.laravel.com">Vapor</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
